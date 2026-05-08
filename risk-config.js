10be6f23ff2a73dfb9efe7a90e05fcf1aabcc274
mygopen <charles@mygopen.com>
2026-05-08 23:12:03 +0800

    Create risk-config.js

---
+++ risk-config.js
@@ -0,0 +1,58 @@
+window.RISK_CONFIG = {
+    socialMediaDomains: [
+        'facebook.com', 'fb.com', 'fb.me', 'instagram.com', 'ig.me',
+        'twitter.com', 'x.com', 'tiktok.com', 'youtube.com', 'youtu.be', 't.me',
+        'threads.net', 'threads.com', 'line.me', 'dcard.tw', 'plurk.com', 'weibo.com', 'xiaohongshu.com'
+    ],
+    highRiskTlds: [
+        '.shop', '.xyz', '.top', '.club', '.live', '.fun', '.store', '.asia',
+        '.digital', '.click', '.site', '.cloud', '.sbs', '.icu', '.cyou',
+        '.chat', '.cn', '.cv', '.gal', '.pro', '.vip', '.cc', '.pw', '.ws'
+    ],
+    commonInternationalTlds: [
+        '.com', '.net', '.org', '.jp', '.kr', '.tw', '.hk', '.dev',
+        '.io', '.ai', '.co', '.app'
+    ],
+    urlShorteners: [
+        'bit.ly', 'goo.gl', 'tinyurl.com', 't.co', 'is.gd', 'buff.ly',
+        'adf.ly', 'ow.ly', 'bit.do', 'su.pr', 'reurl.cc', 'pic.see',
+        'ppt.cc', 'mz.cm', 'i.gal', 'tiny.cc', 't.cn', 'zingala.cc',
+        'aka.ms', 'truthsocial.com', 'l.facebook.com', 'l.instagram.com',
+        'l.messenger.com'
+    ],
+    safeShorteners: [
+        'lihi.io', 'reurl.cc', 'picsee.io', 'pse.is', 'bit.ly',
+        'bitly.com', 'aka.ms'
+    ],
+    freeHostingProviders: [
+        'dudaone.com', 'wixsite.com', 'blogspot.com', 'wordpress.com',
+        'weebly.com', 'github.io', 'vercel.app', 'netlify.app',
+        'herokuapp.com', 'onrender.com', 'glitch.me', 'firebaseapp.com',
+        'pages.dev', 'myshopify.com', 'surge.sh', 'cloudfront.net',
+        'workers.dev', 'web.app', 'azureedge.net', 'ondigitalocean.app',
+        'eu.cc'
+    ],
+    safeCommercePlatforms: [
+        'shoplineapp.com', 'cyberbiz.co', 'meepshoper.com', 'waca.tw',
+        'waca.ec', 'waca.store', 'easystore.co', '91app.com', 'qdm.tw',
+        'boutir.com', 'myshopify.com'
+    ],
+    highRiskRegistrars: [
+        'namesilo', 'namecheap', 'gname', 'alibaba', 'godaddy',
+        'gmo', 'reg.ru', 'dynadot', 'porkbun', 'tucows'
+    ],
+    suspiciousDomainFragments: [
+        '-tw', '-com', '-online'
+    ],
+    fakeServiceKeywords: [
+        'einvoice', 'taipower', 'water', 'fetc'
+    ],
+    scamKeywords: [
+        'ctbc', 'ctbcbank', 'cathay', 'taishin', 'esun', 'fubon', 'post-gov',
+        '7-11-com', 'myship-711', '711-tw', 'familytw', 'famiport', 'myship',
+        'com-tw', '-online', '-verify', '-login'
+    ],
+    sensitiveUrlParams: [
+        'token=', 'session=', 'auth=', 'verify=', 'verification=', 'login='
+    ]
+};
